ecb27e21a9b88a21809415acb9a659d6fd68c15f
kim ha young <158129874+zerozeroha@users.noreply.github.com>
2025-07-21 00:39:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        portfolio: 'portfolio로.html'
+        portfolio: 'portfolio.html'
       }
     }
   }
